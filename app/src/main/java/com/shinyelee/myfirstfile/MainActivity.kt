0486356137cdb4b6e015fc17028c698121348be9
shinyelee <shinye0213@naver.com>
2022-02-10 23:15:43 +0900

    RecyclerView (3)

--- app/src/main/java/com/shinyelee/myfirstfile/MainActivity.kt
+++ app/src/main/java/com/shinyelee/myfirstfile/MainActivity.kt
@@ -2,10 +2,23 @@ package com.shinyelee.myfirstfile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val items = mutableListOf<String>()
+
+        items.add("a")
+        items.add("b")
+        items.add("c")
+
+        val rv = findViewById<RecyclerView>(R.id.rv)
+        val rvAdapter = RVAdapter(items)
+        rv.adapter = rvAdapter
+        rv.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ app/src/main/java/com/shinyelee/myfirstfile/RVAdapter.kt
@@ -0,0 +1,28 @@
+package com.shinyelee.myfirstfile
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RVAdapter(val items : MutableList<String>) : RecyclerView.Adapter<RVAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RVAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.rv_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RVAdapter.ViewHolder, position: Int) {
+        holder.bindItems(items[position])
+    }
+
+    // 전체 리사이클러뷰의 수
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        fun bindItems(item : String) {
+        }
+    }
+}
