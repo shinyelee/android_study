86bf95503a930226ad985536af1012e522555807
shinyelee <shinye0213@naver.com>
2022-02-12 00:50:18 +0900

    Navigation & Fragment (2)

--- app/src/main/java/com/shinyelee/myfirstfile/SecondFragment.kt
+++ app/src/main/java/com/shinyelee/myfirstfile/SecondFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class SecondFragment : Fragment() {
 
@@ -14,7 +16,14 @@ class SecondFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+        view.findViewById<Button>(R.id.btn1).setOnClickListener {
+            it.findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+        view.findViewById<Button>(R.id.btn3).setOnClickListener {
+            it.findNavController().navigate(R.id.action_secondFragment_to_thirdFragment)
+        }
+        return view
     }
 
 }
