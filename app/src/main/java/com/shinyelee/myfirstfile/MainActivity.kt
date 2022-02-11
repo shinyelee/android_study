212d582079be58d37e4b86102af7eac87e37c51c
shinyelee <shinye0213@naver.com>
2022-02-11 17:15:52 +0900

    rollback

--- app/src/main/java/com/shinyelee/myfirstfile/MainActivity.kt
+++ app/src/main/java/com/shinyelee/myfirstfile/MainActivity.kt
@@ -2,31 +2,10 @@ package com.shinyelee.myfirstfile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val items = mutableListOf<String>()
-
-        items.add("a")
-        items.add("b")
-        items.add("c")
-
-        val rv = findViewById<RecyclerView>(R.id.rv)
-        val rvAdapter = RVAdapter(items)
-        rv.adapter = rvAdapter
-        rv.layoutManager = LinearLayoutManager(this)
-        rvAdapter.itemClick = object : RVAdapter.ItemClick {
-            override fun onClick(view: View, position: Int) {
-                Toast.makeText(baseContext, items[position], Toast.LENGTH_LONG).show()
-            }
-
-        }
     }
 }
