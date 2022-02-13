c201b1300255e6af90943b70f4c3d30e20638bb0
shinyelee <shinye0213@naver.com>
2022-02-13 18:51:06 +0900

    rollback

--- app/src/main/java/com/shinyelee/myfirstfile/MainActivity.kt
+++ app/src/main/java/com/shinyelee/myfirstfile/MainActivity.kt
@@ -7,12 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // 1. navigation 만들기
-
-        // 2. fragment 만들고 연결하기
-
-        // 3. 리사이클러뷰 만들기
     }
 
 }
